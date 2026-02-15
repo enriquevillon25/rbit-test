3ecf7e4cd40bff350233ec042f84d8e3f4f76888
enriquevillon25 <enriquevillon2597@gmail.com>
2026-02-15 09:47:48 -0500

    feat: show correct sned

--- utils/sends.js
+++ utils/sends.js
@@ -1,7 +1,6 @@
 export const handleWhatsApp = () => {
   const phone = "34666266295"; // sin el +
-  const message =
-    "Hola, tengo problema técnico con mi curso, ¿podrían ayudarme?";
+  const message = "Hola, tengo un problema técnico, ¿podrían ayudarme?";
   const url = `https://wa.me/${phone}?text=${encodeURIComponent(message)}`;
 
   window.open(url, "_blank");
